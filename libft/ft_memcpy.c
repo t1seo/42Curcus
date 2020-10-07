68fc29861b936a6920ceb1472ba40c9cfa1138f9
Seo Tae-won <t1won.seo@gmail.com>
2020-10-07 10:05:13 +0900

    Revised ft_memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: 아이디 <이메일>                                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/03 11:53:18 by tseo              #+#    #+#             */
-/*   Updated: 2020/10/06 20:15:44 by tseo             ###   ########.fr       */
+/*   Updated: 2020/10/07 10:05:00 by 아이디              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char		*tmp;
-	const char	*s = src;
+	void *ret;
 
-	tmp = dst;
+	if (!dst || !src)
+		return (0);
+	ret = dst;
 	while (n--)
-		*tmp++ = *s++;
-	return (dst);
+		*(char*)dst++ = *(char*)src++;
+	return (ret);
 }
