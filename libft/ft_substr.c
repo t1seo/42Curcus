802147b258550d1e62b6f2523fd899bec5a6c663
Seo Tae-won <t1won.seo@gmail.com>
2020-10-07 12:29:30 +0900

    Added protect to ft_substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 16:09:00 by tseo              #+#    #+#             */
-/*   Updated: 2020/10/06 20:09:35 by tseo             ###   ########.fr       */
+/*   Updated: 2020/10/07 12:28:22 by tseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char *str;
 
-	if (!len || ft_strlen(s) <= start)
+	if (!s || !len || ft_strlen(s) <= start)
 		return (ft_strdup(""));
-	if (!s || !(str = (char*)malloc(sizeof(*s) * (len + 1))))
+	if (!(str = (char*)malloc(sizeof(*s) * (len + 1))))
 		return (0);
 	ft_strlcpy(str, s + start, len + 1);
 	return (str);
