3d0fcfb2870663fc7842cc9be80e29afb22c2e12
Seo Tae-won <t1won.seo@gmail.com>
2020-10-07 12:33:20 +0900

    Added protect to ft_strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 19:20:16 by tseo              #+#    #+#             */
-/*   Updated: 2020/10/06 20:11:26 by tseo             ###   ########.fr       */
+/*   Updated: 2020/10/07 12:32:33 by tseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*str;
 	int		len;
 
+	if (!s1 && !s2)
+		return (0);
 	len = ft_strlen(s1) + ft_strlen(s2);
 	if (!(str = (char*)malloc(sizeof(char) * (len + 1))))
 		return (0);
