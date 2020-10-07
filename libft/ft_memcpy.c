2bcd269797beb686378048a2bde42cd769ce2526
Seo Tae-won <t1won.seo@gmail.com>
2020-10-07 12:51:05 +0900

    norminette

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/03 11:53:18 by tseo              #+#    #+#             */
-/*   Updated: 2020/10/07 10:28:25 by tseo             ###   ########.fr       */
+/*   Updated: 2020/10/07 12:50:46 by tseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void*	ft_memcpy(void *dst, const void* src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	void *ret;
 
