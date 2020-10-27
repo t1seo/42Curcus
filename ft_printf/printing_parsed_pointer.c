b7f7b552ef10e0d488f4a25d19333c35db798aac
Seo Tae-won <t1won.seo@gmail.com>
2020-10-27 19:44:51 +0900

    ft_printf ptr test

--- ft_printf/printing_parsed_pointer.c
+++ ft_printf/printing_parsed_pointer.c
@@ -6,25 +6,7 @@
 /*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/27 15:45:09 by tseo              #+#    #+#             */
-/*   Updated: 2020/10/27 19:35:37 by tseo             ###   ########.fr       */
+/*   Updated: 2020/10/27 19:44:40 by tseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
-
-int		allocate_pointer_data(t_va_info *info, va_list *ap)
-{
-	unsigned long int	tmp;
-	char				*hex_data;
-
-	tmp = (unsigned long)va_arg(*ap, void*);
-	if (!(hex_data = ft_itoa_hex(tmp)))
-		return (0);
-	if (!(info->va_data = ft_strjoin("0x", hex_data)))
-	{
-		free(hex_data);
-		return (0);
-	}
-	free(hex_data);
-	return (1);
-}
