39957432725652c656f4b76ef5899fd9036872dd
Seo Tae-won <t1won.seo@gmail.com>
2020-10-28 21:00:24 +0900

    ft_printf test

--- ft_printf/allocating_numbers_data.c
+++ ft_printf/allocating_numbers_data.c
@@ -6,7 +6,7 @@
 /*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/27 15:44:19 by tseo              #+#    #+#             */
-/*   Updated: 2020/10/28 20:28:03 by tseo             ###   ########.fr       */
+/*   Updated: 2020/10/28 20:59:02 by tseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 char const	*hex_digits_lower = "0123456789abcdef";
 char const	*hex_digits_upper = "0123456789ABCDEF";
+char const	*int_digits = "0123456789";
 
 int		allocate_signed_int(t_va_info *info, va_list *ap)
 {
@@ -30,7 +31,7 @@ int		allocate_unsigned_int(t_va_info *info, va_list *ap)
 	unsigned int tmp;
 
 	tmp = va_arg(*ap, unsigned int);
-	if (!(info->va_data = ft_itoa(tmp)))
+	if (!(info->va_data = ft_itoa_base(tmp, int_digits)))
 		return (0);
 	return (1);
 }
@@ -41,12 +42,12 @@ int		allocate_hex(t_va_info *info, va_list *ap)
 	tmp = va_arg(*ap, unsigned int);
 	if (info->specifier == 'x')
 	{
-		if (!(info->va_data = ft_itoa_hex(tmp, hex_digits_lower)))
+		if (!(info->va_data = ft_itoa_base(tmp, hex_digits_lower)))
 			return (0);
 	}
 	if (info->specifier == 'X')
 	{
-		if (!(info->va_data = ft_itoa_hex(tmp, hex_digits_upper)))
+		if (!(info->va_data = ft_itoa_base(tmp, hex_digits_upper)))
 			return (0);
 	}
 	return (1);
--- ft_printf/utils.c
+++ ft_printf/utils.c
@@ -6,7 +6,7 @@
 /*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/27 15:58:29 by tseo              #+#    #+#             */
-/*   Updated: 2020/10/28 20:12:58 by tseo             ###   ########.fr       */
+/*   Updated: 2020/10/28 20:59:53 by tseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,34 +19,36 @@ void reset_info(t_va_info *info)
 	ft_memset(info, 0, sizeof(t_va_info));
 }
 
-char	*ft_itoa_hex(unsigned long int value, char const *base)
+char	*ft_itoa_base(unsigned long int value, char const *base)
 {
 	unsigned long int	n;
 	int					i;
 	char				*ret;
+	int					len;
 
+	len = ft_strlen(base);
 	n = value;
 	i = 0;
-	if (value == 0 || value < 0)
-		++i;
+	// if (value == 0 || value < 0)
+		// ++i;
 	while (n)
 	{
-		n /= 16;
+		n /= len;
 		++i;
 	}
 	n = value;
 	if (!(ret = (char*)malloc(sizeof(char) * (i + 1))))
 		return (0);
 	ret[i] = 0;
-	if (value < 0)
-		ret[i] = '-';
-	if (value == 0)
-		ret[i] = '0';
+	// if (value < 0)
+	// 	ret[i] = '-';
+	// if (value == 0)
+	// 	ret[i] = '0';
 	while (n)
 	{
 		--i;
-		ret[i] = base[n % 16];
-		n /= 16;
+		ret[i] = base[n % len];
+		n /= len;
 	}
 	return (ret);
 }
