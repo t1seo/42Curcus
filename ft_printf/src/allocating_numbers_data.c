4cf8dd10a7e95085d0d6de442fd7f2831bba22fa
Seo Tae-won <t1won.seo@gmail.com>
2020-10-31 05:17:21 +0900

    bug fixed

--- ft_printf/src/allocating_numbers_data.c
+++ ft_printf/src/allocating_numbers_data.c
@@ -6,7 +6,7 @@
 /*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/27 15:44:19 by tseo              #+#    #+#             */
-/*   Updated: 2020/10/31 05:11:15 by tseo             ###   ########.fr       */
+/*   Updated: 2020/10/31 05:16:30 by tseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,17 @@ int		allocate_signed_int(t_va_info *info, va_list *ap)
 	int tmp;
 
 	tmp = va_arg(*ap, int);
-	if (tmp == 0 && (info->precision == -1 || info->precision == 0))
+	if (tmp == 0 && info->precision == -1)
 	{
 		if (!(info->va_data = (char*)malloc(sizeof(char) * 1)))
 			return (0);
+		info->va_data[0] = 0;
+	}
+	else if (tmp == 0)
+	{
+		if (!(info->va_data = (char*)malloc(sizeof(char) * 2)))
+			return (0);
+		info->va_data[0] = '0';
 		info->va_data[0] = 0;
 	}
 	else
@@ -40,10 +47,17 @@ int		allocate_unsigned_int(t_va_info *info, va_list *ap)
 	unsigned int tmp;
 
 	tmp = va_arg(*ap, unsigned int);
-	if (tmp == 0 && (info->precision == -1 || info->precision == 0))
+	if (tmp == 0 && info->precision == -1)
 	{
 		if (!(info->va_data = (char*)malloc(sizeof(char) * 1)))
 			return (0);
+		info->va_data[0] = 0;
+	}
+	else if (tmp == 0)
+	{
+		if (!(info->va_data = (char*)malloc(sizeof(char) * 2)))
+			return (0);
+		info->va_data[0] = '0';
 		info->va_data[0] = 0;
 	}
 	else
@@ -59,7 +73,7 @@ int		allocate_hex(t_va_info *info, va_list *ap)
 	unsigned int tmp;
 
 	tmp = va_arg(*ap, unsigned int);
-	if (tmp == 0 && (info->precision == -1 || info->precision == 0))
+	if (tmp == 0 && info->precision == -1)
 	{
 		if (!(info->va_data = (char*)malloc(sizeof(char) * 1)))
 			return (0);
