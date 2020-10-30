c86005a2a54dc410b730f2a3200291803cbef08c
Seo Tae-won <t1won.seo@gmail.com>
2020-10-31 01:55:29 +0900

    fixed bugs

--- ft_printf/src/allocating_chars_data.c
+++ ft_printf/src/allocating_chars_data.c
@@ -6,7 +6,7 @@
 /*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/27 15:41:55 by tseo              #+#    #+#             */
-/*   Updated: 2020/10/31 01:40:33 by tseo             ###   ########.fr       */
+/*   Updated: 2020/10/31 01:55:21 by tseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,20 @@ int		allocate_char_data(t_va_info *info, va_list *ap)
 {
 	char tmp;
 
-	if (!(info->va_data = (char*)malloc(sizeof(char) * 2)))
-		return (0);
 	tmp = va_arg(*ap, int);
 	if (!tmp)
-		info->va_data[0] = '\x00';
+	{
+		if (!(info->va_data = (char*)malloc(sizeof(char) * 1)))
+			return (0);
+		info->va_data[0] = 0;
+	}
 	else
+	{
+		if (!(info->va_data = (char*)malloc(sizeof(char) * 2)))
+			return (0);
 		info->va_data[0] = tmp;
-	info->va_data[1] = 0;
+		info->va_data[1] = 0;
+	}
 	return (1);
 }
 
