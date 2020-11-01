2e086d0076a919dc10c107052b82db14f63b60a7
Seo Tae-won <t1won.seo@gmail.com>
2020-11-01 17:30:38 +0900

    ft_printf - final test

--- ft_printf/src/printing_num_data.c
+++ ft_printf/src/printing_num_data.c
@@ -6,59 +6,75 @@
 /*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/31 22:01:01 by tseo              #+#    #+#             */
-/*   Updated: 2020/10/31 23:48:59 by tseo             ###   ########.fr       */
+/*   Updated: 2020/11/01 17:27:40 by tseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ft_printf.h"
 
-void	print_num_data(t_va_info *info, int *count)
+static void		print_num_data_without_precision(t_va_info *info, int va_data_len,
+													int *count, int skip_sign_idx)
 {
-	int len;
-	int rest_width;
-
-	len = ft_strlen(info->va_data);
-	if (info->precision == -1)
+	if (info->flag == '-')
 	{
-		if (info->flag == '-')
-		{
-			write_letters(info->va_data, len, count);
-			write_padding(' ', info->width - len, count);
-		}
-		else if (info->flag == '0')
-		{
-			if (info->va_data[0] == '-')
-			{
-				write_letters(info->va_data, 1, count);
-				write_padding(info->flag, info->width - len, count);
-				write_letters(info->va_data + 1, len - 1, count);
-			}
-			else
-			{
-				write_padding(info->flag, info->width - len, count);
-				write_letters(info->va_data, len, count);
-			}
-		}
+		write_letters(info->va_data, va_data_len, count);
+		write_padding(' ', info->width - va_data_len, count);
+	}
+	else if (info->flag == '0')
+	{
+		write_letters(info->va_data, skip_sign_idx, count);
+		write_padding(info->flag, info->width - va_data_len, count);
+		write_letters(info->va_data + skip_sign_idx, va_data_len - skip_sign_idx, count);
 	}
 	else
 	{
-		if (info->flag == '-')
-		{
-			if (info->va_data[0] == '-')
-			{
-			}
-			else
-			{
-			}
-		}
-		else
-		{
-			if (info->va_data[0] == '-')
-			{
-			}
-			else
-			{
-			}
-		}
+		write_padding(info->flag, info->width - va_data_len, count);
+		write_letters(info->va_data, va_data_len, count);
+	}
+}
+
+static void		print_num_data_with_precision(t_va_info *info, int va_data_len,
+													int *count, int skip_sign_idx)
+{
+	int rest_width_len;
+
+	if (va_data_len > info->precision - skip_sign_idx)
+		rest_width_len = info->width - va_data_len;
+	else
+		rest_width_len = info->width - (info->precision + skip_sign_idx);
+	if (info->flag == '-')
+	{
+		write_letters(info->va_data, skip_sign_idx, count);
+		write_padding('0', info->precision - (va_data_len - skip_sign_idx), count);
+		write_letters(info->va_data + skip_sign_idx, va_data_len - skip_sign_idx, count);
+		write_padding(' ', rest_width_len, count);
+	}
+	else
+	{
+		write_padding(' ', rest_width_len, count);
+		write_letters(info->va_data, skip_sign_idx, count);
+		write_padding('0', info->precision - (va_data_len - skip_sign_idx), count);
+		write_letters(info->va_data + skip_sign_idx, va_data_len - skip_sign_idx, count);
+	}
+}
+
+void	print_num_data(t_va_info *info, int *count)
+{
+	int va_data_len;
+	int	skip_sign_idx;
+
+	va_data_len = ft_strlen(info->va_data);
+	if (info->va_data[0] == '-')
+		skip_sign_idx = 1;
+	else
+		skip_sign_idx = 0;
+
+	if (info->precision == -1)
+		print_num_data_without_precision(info, va_data_len, count, skip_sign_idx);
+	else
+	{
+		if (info->precision == 0 && info->va_data[0] == '0' && va_data_len == 1)
+			va_data_len = 0;
+		print_num_data_with_precision(info, va_data_len, count, skip_sign_idx);
 	}
 }
--- ft_printf/src/printing_ptr_data.c
+++ ft_printf/src/printing_ptr_data.c
@@ -6,7 +6,7 @@
 /*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/31 21:39:06 by tseo              #+#    #+#             */
-/*   Updated: 2020/10/31 22:23:43 by tseo             ###   ########.fr       */
+/*   Updated: 2020/11/01 17:11:18 by tseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,20 @@
 
 void	print_ptr_data(t_va_info *info, int *count)
 {
-	int len;
+	int va_data_len;
 
-	len = ft_strlen(info->va_data);
-	if (info->precision == 0 && !ft_strncmp(info->va_data, "0x0", 3))
-		len--;
+	va_data_len = ft_strlen(info->va_data);
+	if (info->precision == 0 && !ft_strncmp(info->va_data, "0x0", 3)
+		&& va_data_len == 3)
+		va_data_len--;
 	if (info->flag == '-')
 	{
-		write_letters(info->va_data, len, count);
-		write_padding(' ', info->width - len, count);
+		write_letters(info->va_data, va_data_len, count);
+		write_padding(' ', info->width - va_data_len, count);
 	}
 	else
 	{
-		write_padding(' ', info->width - len, count);
-		write_letters(info->va_data, len, count);
+		write_padding(' ', info->width - va_data_len, count);
+		write_letters(info->va_data, va_data_len, count);
 	}
 }
