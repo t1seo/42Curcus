8dfa7850be3531d8e604c7c8456ce21b25deb915
Seo Tae-won <t1won.seo@gmail.com>
2020-11-23 19:07:36 +0900

    updated

--- exam02/ft_printf/ft_printf.c
+++ exam02/ft_printf/ft_printf.c
@@ -1,271 +1,398 @@
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-// 9. 진수 숫자 출력 함수
-void ft_putnbr_base(long long num, int base)
+typedef struct s_va_info
 {
-	char *dec = "0123456789";
-	char *hex = "0123456789abcdef";
+	char flag;
+	int width;
+	int precision;
+	char specifier;
+	char *va_data;
+} t_va_info;
 
-	if (num < 0)
-		num = -num;
-	if (num >= base)
-	{
-		ft_putnbr_base(num / base, base);
-		ft_putnbr_base(num % base, base);
-	}
-	else
-	{
-		if (base == 10)
-			write(1, &dec[num], 1);
-		else if (base == 16)
-			write(1, &hex[num], 1);
-	}
-}
+char const *g_format_type = "sdx";
+char const *g_hex_digits_lower = "0123456789abcdef";
+char const *g_int_digits = "0123456789";
 
-// 8-1. 숫자 길이 함수
-int ft_numlen(int num)
+/*
+** libft
+*/
+
+size_t ft_strlen(const char *s)
 {
-	int i = 1;
-	while (num /= 10)
-		i++;
-	return (i);
-}
+	size_t i;
 
-// 8-2. 16진수 숫자 길이 함수
-int ft_hexlen(unsigned int num)
-{
-	int i = 1;
-	while (num /= 16)
-		i++;
-	return (i);
-}
-
-// 7. n 길이만큼 문자열 출력
-void ft_putnstr(char *s, int n)
-{
-	int i = 0;
-	while (s[i] && i < n)
-	{
-		write(1, &s[i], 1);
-		i++;
-	}
-}
-
-// 6. 문자열 길이 구하는 함수
-int ft_strlen(char *s)
-{
-	int i = 0;
+	i = 0;
 	while (s[i])
 		i++;
 	return (i);
 }
-
-// 5. with dot precision 처리하고 출력해주는 함수
-// f: format
-// conversion : s, d, x
-// ap
-int print_with_flags(char *f, char conversion, va_list ap)
+static int ft_abs(int n)
 {
-	int width = 0;
-	int dot = 0;
-	int precision = 0;
-	int nprinted = 0; // 이 함수의 리턴값으로 출력한 문자 갯수 의미
-	int i = 0;
-
-	// width, dot, precision 계산
-	while (f[i])
-	{
-		// width
-		if (('0' <= f[i] && f[i] <= '9') && !dot)
-			width = (width * 10) + (f[i] - 48);
-		// dot
-		else if (f[i] == '.')
-			dot = 1;
-		else if (('0' <= f[i] && f[i] <= '9') && dot)
-			precision = (precision * 10) + (f[i] - 48);
-		i++;
-	}
-
-	char *str = 0;
-	int num = 0;
-	unsigned int unum = 0;
-	int len = 0;
-
-	// s 처리
-	if (conversion == 's')
-	{
-		if (!(str = va_arg(ap, char *)))
-			str = "(null)";
-		len = ft_strlen(str);
-		if (dot)
-		{
-			if (len < precision) // precision이 문자열 길이보다 작으면
-				precision = len;
-		}
-		else // . 이 없으면 pre = 0이기 때문에
-			precision = len;
-		i = 0;
-		// width - precision 만큼 공백 출력
-		while (i < width - precision)
-		{
-			write(1, " ", 1);
-			nprinted++; // 공백 개수만큼 nprinted++
-			i++;
-		}
-		nprinted += precision;		// 출력할 문자 개수만큼 nprinted++
-		ft_putnstr(str, precision); // precision 만큼 문자 출력
-	}
-
-	// d, x 처리
-	else if (conversion == 'd' || conversion == 'x')
-	{
-		// d 라면
-		if (conversion == 'd')
-		{
-			num = va_arg(ap, int);
-			len = ft_numlen(num);
-		}
-		// x 라면
-		else if (conversion == 'x')
-		{
-			unum = va_arg(ap, unsigned int); // unsigned int
-			len = ft_hexlen(unum);
-		}
-		// 예외처리 0.0
-		if (num == 0 && unum == 0 && dot && precision == 0)
-		{
-			i = 0;
-			while (i < width)
-			{
-				write(1, " ", 1);
-				nprinted++;
-				i++;
-			}
-			return (nprinted);
-		}
-		// precision이 숫자 길이보다 작다면
-		if (precision < len)
-			precision = len;
-		if (num < 0) // 음수라면 공백처리
-			precision++;
-		i = 0;
-		// width - precision 만큼 공백 출력
-		while (i < width - precision)
-		{
-			write(1, " ", 1);
-			nprinted++;
-			i++;
-		}
-		// 음수이면 '-' 출력
-		if (num < 0)
-		{
-			write(1, "-", 1);
-			len++;
-		}
-		i = 0;
-		// precision - len(숫자 길이)만큼 0 출력
-		while (i < precision - len)
-		{
-			write(1, "0", 1);
-			i++;
-		}
-		nprinted += precision; // nprinted에 precision 만큼 더해준다.
-		// 숫자 출력
-		if (conversion == 'd')
-			ft_putnbr_base(num, 10);
-		else if (conversion == 'x')
-			ft_putnbr_base(unum, 16);
-	}
-	free(f);		   // format을 free 해준다
-	return (nprinted); // 출력한 글자 수 리턴
+	return (n < 0) ? -n : n;
 }
 
-// 4. n 길이 만큼 새로운 문자열 할당 함수
-char *ft_strndup(char *s, int n)
+int ft_isdigit(int c)
+{
+	return ('0' <= c && c <= '9');
+}
+
+static int get_num_len(int n)
+{
+	int len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n != 0)
+	{
+		len++;
+		n /= 10;
+	}
+	return (len);
+}
+
+int ft_atoi(const char *str)
 {
 	int i;
-	char *ptr;
+	int n;
+	int sign;
 
-	if (!(ptr = (char *)malloc(sizeof(char) * (n + 1))))
+	i = 0;
+	n = 0;
+	sign = 1;
+	while ((9 <= str[i] && str[i] <= 13) || str[i] == ' ')
+		i++;
+	sign = (str[i] == '-') ? -1 : 1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while ('0' <= str[i] && str[i] <= '9')
+	{
+		n = 10 * n + (str[i] - '0');
+		i++;
+	}
+	return (n * sign);
+}
+
+char *ft_strdup(const char *s)
+{
+	int i;
+	char *ret;
+
+	if (!(ret = malloc(sizeof(char) * (ft_strlen(s) + 1))))
 		return (0);
 	i = 0;
-	while (s[i] && i < n)
-	{
-		ptr[i] = s[i];
-		i++;
-	}
-	ptr[i] = 0;
-	return (ptr);
-}
-
-// 3. 서식자의 위치를 찾아주는 함수
-char *find_idx(char *s)
-{
-	int i;
-	i = 0;
-	// 문자가 존재하고 숫자나 점인 경우 idx++
-	while (s[i] && (('0' <= s[i] && s[i] <= '9') || s[i] == '.'))
-		i++;
-	// 출력 가능한 문자이고
-	if (32 <= s[i] && s[i] <= 126)
-	{
-		// 서식문자라면
-		if (s[i] == 's' || s[i] == 'd' || s[i] == 'x')
-			return (s + i); // 서식 문자 위치 반한
-		else
-			return (0);
-	}
-	return (0);
-}
-
-// 2. %를 찾아서 처리해주는 함수
-int check_format(va_list ap, char *s)
-{
-	int i;
-	char *conversion; // %의 위치 반환
-	int ret;
-	char *format;
-
-	ret = 0;
-	i = 0;
 	while (s[i])
 	{
-		// %를 만나게 되면
-		if (s[i] == '%')
-		{
-			conversion = find_idx(s + i + 1); // 서식 문자 s, d, x를 찾는다
-			// 서식 문자를 찾으면
-			if (conversion)
-			{
-				format = ft_strndup(s + i, (conversion + 1) - (s + i)); // formatting 할 내용을 담는다
-				ret += print_with_flags(format, *conversion, ap);		// 출력
-				i += ((conversion) - (s + i));							// 인덱스 이동
-			}
-			else // s, d, x가 없을 때
-				i++;
-		}
-		else // % 없을 때 문자 출력
-		{
-			write(1, &s[i], 1);
-			ret++;
-		}
+		ret[i] = s[i];
 		i++;
+	}
+	ret[i] = 0;
+	return (ret);
+}
+
+/*
+** utils
+*/
+
+void reset_info(t_va_info *info)
+{
+	if (!(info->va_data))
+		free(info->va_data);
+	info->flag = 0;
+	info->width = -1;
+	info->precision = -1;
+	info->specifier = 0;
+	info->va_data = 0;
+}
+
+char *ft_itoa_base(unsigned long int value, char const *base)
+{
+	unsigned long int n;
+	int i;
+	char *ret;
+	int len;
+
+	len = ft_strlen(base);
+	n = value;
+	i = 0;
+	while (n)
+	{
+		n /= len;
+		++i;
+	}
+	n = value;
+	if (!(ret = (char *)malloc(sizeof(char) * (i + 1))))
+		return (0);
+	ret[i] = 0;
+	while (n)
+	{
+		--i;
+		ret[i] = base[n % len];
+		n /= len;
 	}
 	return (ret);
 }
 
-// 1. ft_printf 함수
-int ft_printf(const char *s, ...)
+void write_letters(char *str, int len, int *count)
+{
+	write(1, str, len);
+	(*count) += len;
+}
+
+void write_padding(char padding, int size, int *count)
+{
+	if (padding == 0)
+		padding = ' ';
+	while (size > 0)
+	{
+		write(1, &padding, 1);
+		(*count)++;
+		size--;
+	}
+}
+
+/*
+** parsing_format
+*/
+void parsing_width(const char **ptr, t_va_info *info, va_list *ap)
+{
+	info->width = ft_atoi(*ptr);
+	while (ft_isdigit(**ptr))
+		(*ptr)++;
+}
+
+void parsing_precision(const char **ptr, t_va_info *info, va_list *ap)
+{
+	if (**ptr != '.')
+	{
+		info->precision = -1;
+		return;
+	}
+	(*ptr)++;
+	info->precision = ft_atoi(*ptr);
+	while (ft_isdigit(**ptr))
+		(*ptr)++;
+}
+
+int parsing_specifier(const char **ptr, t_va_info *info)
+{
+	const char *format_type;
+
+	format_type = g_format_type;
+	while (*format_type)
+	{
+		if (*format_type == **ptr)
+		{
+			info->specifier = *format_type;
+			(*ptr)++;
+			return (1);
+		}
+		format_type++;
+	}
+	return (0);
+}
+
+/*
+** allocating data
+*/
+
+int allocate_str_data(t_va_info *info, va_list *ap)
+{
+	char *tmp;
+
+	tmp = va_arg(*ap, char *);
+	if (!tmp)
+	{
+		if (!(info->va_data = ft_strdup("(null)")))
+			return (0);
+		return (1);
+	}
+	if (!(info->va_data = ft_strdup(tmp)))
+		return (0);
+	return (1);
+}
+
+int allocate_signed_int(t_va_info *info, va_list *ap)
+{
+	int tmp;
+
+	tmp = va_arg(*ap, int);
+	if (tmp == 0 && info->precision == 0)
+	{
+		if (!(info->va_data = (char *)malloc(sizeof(char) * 1)))
+			return (0);
+		info->va_data[0] = 0;
+	}
+	else if (tmp == 0)
+	{
+		if (!(info->va_data = (char *)malloc(sizeof(char) * 2)))
+			return (0);
+		info->va_data[0] = '0';
+		info->va_data[1] = 0;
+	}
+	else
+	{
+		if (!(info->va_data = ft_itoa_base(tmp, g_int_digits)))
+			return (0);
+	}
+	return (1);
+}
+
+int allocate_hex(t_va_info *info, va_list *ap)
+{
+	unsigned int tmp;
+
+	tmp = va_arg(*ap, unsigned int);
+	if (tmp == 0)
+	{
+		if (!(info->va_data = (char *)malloc(sizeof(char) * 2)))
+			return (0);
+		info->va_data[0] = '0';
+		info->va_data[1] = 0;
+	}
+	else if (info->specifier == 'x')
+	{
+		if (!(info->va_data = ft_itoa_base(tmp, g_hex_digits_lower)))
+			return (0);
+	}
+
+	return (1);
+}
+
+/*
+** printing data
+*/
+
+void print_str_data(t_va_info *info, int *count)
+{
+	int va_data_len;
+
+	va_data_len = ft_strlen(info->va_data);
+	if (info->precision != -1 && info->precision < va_data_len)
+		va_data_len = info->precision;
+	write_padding(' ', info->width - va_data_len, count);
+	write_letters(info->va_data, va_data_len, count);
+}
+
+static void print_num_data_without_precision(t_va_info *info,
+											 int va_data_len, int *count, int skip_sign_idx)
+{
+	write_padding(info->flag, info->width - va_data_len, count);
+	write_letters(info->va_data, va_data_len, count);
+}
+
+static void print_num_data_with_precision(t_va_info *info, int va_data_len,
+										  int *count, int skip_sign_idx)
+{
+	int rest_width_len;
+
+	if (va_data_len > info->precision + skip_sign_idx)
+		rest_width_len = info->width - va_data_len;
+	else
+		rest_width_len = info->width - (info->precision + skip_sign_idx);
+
+	write_padding(' ', rest_width_len, count);
+	write_letters(info->va_data, skip_sign_idx, count);
+	write_padding('0',
+				  info->precision - (va_data_len - skip_sign_idx), count);
+	write_letters(info->va_data + skip_sign_idx,
+				  va_data_len - skip_sign_idx, count);
+}
+
+void print_num_data(t_va_info *info, int *count)
+{
+	int va_data_len;
+	int skip_sign_idx;
+
+	va_data_len = ft_strlen(info->va_data);
+	if (info->va_data[0] == '-')
+		skip_sign_idx = 1;
+	else
+		skip_sign_idx = 0;
+	if (info->precision == -1)
+		print_num_data_without_precision(info, va_data_len,
+										 count, skip_sign_idx);
+	else
+	{
+		if (info->precision == 0 && info->va_data[0] == '0' && va_data_len == 1)
+			va_data_len = 0;
+		print_num_data_with_precision(info, va_data_len, count, skip_sign_idx);
+	}
+}
+
+/*
+** ft_printf
+*/
+static int parsing_format(const char **ptr, t_va_info *info, va_list *ap)
+{
+	(*ptr)++;
+	parsing_width(ptr, info, ap);
+	parsing_precision(ptr, info, ap);
+	if (!(parsing_specifier(ptr, info)))
+		return (0);
+	return (1);
+}
+
+static int data_allocation(t_va_info *info, va_list *ap)
+{
+	if (info->specifier == 's')
+		return (allocate_str_data(info, ap));
+	if (info->specifier == 'd')
+		return (allocate_signed_int(info, ap));
+	if (info->specifier == 'x')
+		return (allocate_hex(info, ap));
+	return (1);
+}
+
+static void print_data(t_va_info *info, int *count)
+{
+	if (info->specifier == 's')
+		print_str_data(info, count);
+	if (info->specifier == 'd' || info->specifier == 'x')
+		print_num_data(info, count);
+}
+
+static int init_format_parsing(const char *format, va_list *ap,
+							   t_va_info *info)
+{
+	int count;
+
+	count = 0;
+	while (*format)
+	{
+		reset_info(info);
+		if ((*format) == '%')
+		{
+			if (!(parsing_format(&format, info, ap)) || !(data_allocation(info, ap)))
+			{
+				count = -1;
+				break;
+			}
+			print_data(info, &count);
+			continue;
+		}
+		write(1, format++, 1);
+		count++;
+	}
+	reset_info(info);
+	free(info);
+	return (count);
+}
+
+int ft_printf(const char *format, ...)
 {
 	int ret;
 	va_list ap;
+	t_va_info *info;
 
-	if (!s)
+	if (!(info = (t_va_info *)malloc(sizeof(t_va_info))))
 		return (-1);
-	va_start(ap, s);
-	ret = check_format(ap, (char *)s);
+	if (!ft_strlen(format))
+		return (0);
+	va_start(ap, format);
+	ret = init_format_parsing(format, &ap, info);
 	va_end(ap);
 	return (ret);
 }
--- exam02/ft_printf/main.c
+++ exam02/ft_printf/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "ft_printf.h"
 
 int ft_printf(char const *format, ...);
 
