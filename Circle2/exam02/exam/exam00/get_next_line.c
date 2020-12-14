d088ae7f77cd723df335f838d42f99111d51bcfc
Seo Tae-won <t1won.seo@gmail.com>
2020-12-14 15:51:27 +0900

    exam02 updated

---
+++ Circle2/exam02/exam/exam00/get_next_line.c
@@ -0,0 +1,124 @@
+#include "get_next_line.h"
+
+int		ft_strlen(char *s)
+{
+	int i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	if (!s)
+		return (0);
+	while (*s != (unsigned char)c)
+	{
+		if (*s == 0)
+			return (0);
+		s++;
+	}
+	return (s);
+}
+
+char	*ft_strdup(char *s)
+{
+	if (!s)
+		return (0);
+	int len = ft_strlen(s);
+	char *ret;
+	if (!(ret = (char *)malloc(sizeof(char) * (len + 1))))
+		return (0);
+
+	int i = 0;
+	while (s[i])
+	{
+		ret[i] = s[i];
+		i++;
+	}
+	ret[i] = 0;
+	return (ret);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	if (!s1 && !s2)
+		return (0);
+	int len = ft_strlen(s1) + ft_strlen(s2);
+	char *ret;
+	if (!(ret = (char *)malloc(sizeof(char) * (len + 1))))
+		return (0);
+
+	int i = -1;
+	if (ret)
+	{
+		while (*s1)
+			ret[++i] = *s1++;
+		while (*s2)
+			ret[++i] = *s2++;
+	}
+	ret[++i] = 0;
+	return (ret);
+}
+
+char	*ft_strappend(char *s1, char *s2)
+{
+	char *tmp;
+
+	if (!s1)
+	{
+		s1 = ft_strdup(s2);
+		return (s1);
+	}
+	else
+	{
+		tmp = ft_strjoin(s1, s2);
+		free(s1);
+		s1 = tmp;
+		return (s1);
+	}
+}
+
+int		get_line(char **strs, char **line, char *ptr)
+{
+	char *tmp;
+
+	if (ptr != 0)
+	{
+		*ptr = 0;
+		*line = ft_strdup(*strs);
+		tmp = ft_strdup(ptr + 1);
+		free(*strs);
+		*strs = tmp;
+		return (1);
+	}
+	if (*strs != 0)
+	{
+		*line = *strs;
+		*strs = 0;
+	}
+	else
+		*line = ft_strdup("");
+	return (0);
+}
+
+int		get_next_line(char **line)
+{
+	static char	*strs;
+	char		buf[BUFFER_SIZE + 1];
+	char	*ptr;
+	int		rd_size;
+
+	if (!line || BUFFER_SIZE < 1)
+		return (-1);
+
+	while ((ptr = ft_strchr(strs, '\n')) == 0 && (rd_size = read(0, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[rd_size] = 0;
+		strs = ft_strappend(strs, buf);
+	}
+	if (rd_size < 0)
+		return (-1);
+	return (get_line(&strs, line, ptr));
+}
+
--- Circle2/exam02/ft_printf/old_version5/test.c
+++ Circle2/exam02/ft_printf/old_version5/test.c
@@ -1,25 +1,23 @@
-/*
-** 0. Define Data Types
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
 
-typedef struct	s_info
+/*
+** 0. Define Data Types
+*/
+typedef struct s_info
 {
 	va_list ap;
 	int i;
-	int ret;
-
 	int len;
 	int width;
 	int precision;
 	int precision_width;
-}				t_info;
+	int ret;
+} t_info;
 
-void	reset_struct(t_info *info)
+void reset_struct(t_info *info)
 {
 	info->len = 0;
 	info->width = 0;
@@ -27,26 +25,26 @@ void	reset_struct(t_info *info)
 	info->precision_width = 0;
 }
 
-char	*int_num = "0123456789";
-char	*hex_num = "0123456789abcdef";
+char *int_num = "0123456789";
+char *hex_num = "0123456789abcdef";
 
 /*
-** 1. Library Functions
+**  1. Libary Functions
 **
-** 1) ft_strlen
-** 2) ft_putchar
-** 3) ft_putstr_len
+**	1) ft_strlen
+** 	2) ft_putchar
+**	3) ft_putstr_len
 **
-** 4) nbr_len_base
-** 5) ft_putnbr_base
-** 6) ft_atoi
+**	4) nbr_len_base
+**	5) ft_putnbr_base
+**	6) ft_atoi
 **
-** 7) is_conv
-** 8) print_space
-** 9) print_zero
+**	7) is_conv
+**	8) print_space
+**	9) print_zero
 */
 
-int		ft_strlen(char *s)
+int ft_strlen(char *s)
 {
 	int i = 0;
 	while (s[i])
@@ -54,17 +52,17 @@ int		ft_strlen(char *s)
 	return (i);
 }
 
-void	ft_putchar(char ch)
+void ft_putchar(char ch)
 {
 	write(1, &ch, 1);
 }
 
-void	ft_putstr_len(char *s, int len)
+void ft_putstr_len(char *s, int len)
 {
 	write(1, s, len);
 }
 
-int		nbr_len_base(intmax_t nb, char *base)
+int nbr_len_base(intmax_t nb, char *base)
 {
 	int len = 1;
 	int base_len = ft_strlen(base);
@@ -79,7 +77,7 @@ int		nbr_len_base(intmax_t nb, char *base)
 	return (len);
 }
 
-void	ft_putnbr_base(intmax_t nb, char *base)
+void ft_putnbr_base(intmax_t nb, char *base)
 {
 	int base_len = ft_strlen(base);
 
@@ -90,24 +88,24 @@ void	ft_putnbr_base(intmax_t nb, char *base)
 	ft_putchar(base[nb % base_len]);
 }
 
-int		ft_atoi(char *str, t_info *info)
+int ft_atoi(char *str, t_info *info)
 {
-	int num = 0;
+	int atoi = 0;
 	while ('0' <= str[info->i] && str[info->i] <= '9')
 	{
-		num = num * 10 + str[info->i] - '0';
-		info->i++;
+		atoi = atoi * 10 + str[info->i] - 48;
+		info->i++; // 인덱스 ++
 	}
-	info->i--;
-	return (num);
+	info->i--; // 인덱스 -1
+	return (atoi);
 }
 
-int		is_conv(char c)
+int is_conv(char c)
 {
 	return (c == 's' || c == 'x' || c == 'd');
 }
 
-void	print_space(t_info *info, int len)
+void print_space(t_info *info, int len)
 {
 	int i = 0;
 
@@ -121,7 +119,7 @@ void	print_space(t_info *info, int len)
 	info->ret += len;
 }
 
-void	print_zero(t_info *info, int len)
+void print_zero(t_info *info, int len)
 {
 	int i = 0;
 
@@ -135,12 +133,12 @@ void	print_zero(t_info *info, int len)
 	info->ret += len;
 }
 
-/*
-** 3. convert
-*/
-void	convert_str(t_info *info)
+// 3
+void convert_str(t_info *info)
 {
-	char *str = va_arg(info->ap, char *);
+	char *str;
+
+	str = va_arg(info->ap, char *);
 	if (!str)
 		str = "(null)";
 	info->len = ft_strlen(str);
@@ -151,57 +149,63 @@ void	convert_str(t_info *info)
 	info->ret += info->len;
 }
 
-void	convert_int(t_info *info)
+void convert_int(t_info *info)
 {
-	intmax_t nb = va_arg(info->ap, int);
-	int	zero_len = 0;
-	info->len = nbr_len_base(nb, "0123456789");
+	intmax_t nb;
+	int sp;
 
+	sp = 0;
+	nb = va_arg(info->ap, int);
+	info->len = nbr_len_base(nb, "0123456789");
 	if (nb == 0)
 		info->len = 0;
 	if (info->precision && info->precision_width > info->len)
-		zero_len = info->precision_width - info->len;
+		sp = info->precision_width - info->len;
 	if (nb < 0)
 		info->len += 1;
-	if (nb == 0 && info->precision == 0)
+	if (nb == 0 && !info->precision)
 		info->len = 1;
-
-	print_space(info, info->width - (zero_len + info->len));
+	print_space(info, info->width - (sp + info->len));
 	if (nb < 0)
 		ft_putchar('-');
-	print_zero(info, zero_len);
+	print_zero(info, sp);
 	if (info->precision && nb == 0)
 		return;
-	ft_putnbr_base(nb, int_num);
+	ft_putnbr_base(nb, "0123456789");
 	info->ret += info->len;
 }
 
-void	convert_hex(t_info *info)
+void convert_hex(t_info *info)
 {
-	intmax_t nb = va_arg(info->ap, unsigned int);
-	int zero_len = 0;
-	info->len = nbr_len_base(nb, hex_num);
+	intmax_t nb;
+	int sp;
 
+	sp = 0;
+	nb = va_arg(info->ap, unsigned int);
+	info->len = nbr_len_base(nb, "0123456789abcdef");
 	if (info->precision && info->precision_width > info->len)
-		zero_len = info->precision_width - info->len;
-	if (nb == 0 && info->precision == 1 && info->precision_width == 0)
+		sp = info->precision_width - info->len;
+	if (nb == 0 && !info->precision_width && info->precision == 1)
 		info->len = 0;
-	print_space(info, info->width - (zero_len + info->len));
-	print_zero(info, zero_len);
+	print_space(info, info->width - (sp + info->len));
+	print_zero(info, sp);
 	if (info->precision && info->precision_width == 0 && nb == 0)
 		return;
-	ft_putnbr_base(nb, hex_num);
+	ft_putnbr_base(nb, "0123456789abcdef");
 	info->ret += info->len;
 }
 
 /*
-** 2. parse, ft_printf
+** 2. ft_printf
+** 1) ft_printf
+** 2) parse
 */
-void	parse(char *str, t_info *info)
+
+void parse(char *str, t_info *info)
 {
 	info->i++;
 	reset_struct(info);
-
+	// parsing width and precision
 	while (!is_conv(str[info->i]))
 	{
 		if ('0' <= str[info->i] && str[info->i] <= '9')
@@ -211,9 +215,9 @@ void	parse(char *str, t_info *info)
 			else
 				info->precision_width = ft_atoi(str, info);
 		}
-
+		// precision flags
 		if (str[info->i] == '.')
-			info->precision = 1;
+			info->precision = 1;		 // used when printing
 		info->i++;
 	}
 	str[info->i] == 's' ? convert_str(info) : 0;
@@ -221,7 +225,7 @@ void	parse(char *str, t_info *info)
 	str[info->i] == 'x' ? convert_hex(info) : 0;
 }
 
-int		ft_printf(const char *str, ...)
+int ft_printf(const char *str, ...)
 {
 	t_info info;
 
@@ -243,3 +247,4 @@ int		ft_printf(const char *str, ...)
 	return (info.ret);
 }
 
+// clear
