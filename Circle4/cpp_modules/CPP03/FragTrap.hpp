84b61a44e2188da16dfef0e42eb8aac48aed739c
Tae-won Seo <t1won.seo@gmail.com>
2021-05-26 13:33:16 +0900

    cpp03 - ex00

---
+++ Circle4/cpp_modules/CPP03/FragTrap.hpp
@@ -0,0 +1,50 @@
+#ifndef FRAGTRAP
+#define FRAGTRAP
+
+#include <iostream>
+
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+class FragTrap
+{
+public:
+    FragTrap(void);
+    FragTrap(const std::string &name);
+    FragTrap(const FragTrap &fragTrap);
+    ~FragTrap();
+
+    FragTrap &operator=(const FragTrap &fragTrap);
+
+    void rangedAttack(std::string const &target);
+    void meleeAttack(std::string const &target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+    void vaulthunter_dot_exe(std::string const &target);
+
+    int getHitPoint() const;
+    int getEnergyPoint() const;
+    void showStatus();
+
+private:
+    std::string mName;
+    int mHitPoint;
+    int mMaxHitPoint;
+    int mEnergyPoint;
+    int mMaxEnergyPoint;
+    int mLevel;
+    int mMeleeAttackDamage;
+    int mRangedAttackDamage;
+    int mArmorDamageReduction;
+
+    static std::string mAttackList[5];
+    static int mAttackEnergy[5];
+    static int mAttackDamage[5];
+};
+
+#endif
