b9465b1d913cdf7c729689b069eb87cf3ac8cd26
Tae-won Seo <t1won.seo@gmail.com>
2021-06-11 15:00:52 +0900

    cpp03 done

--- Circle4/cpp_modules/CPP03/ex01/main.cpp
+++ Circle4/cpp_modules/CPP03/ex01/main.cpp
@@ -6,6 +6,9 @@ int main(void)
     ScavTrap alpha("alpha");
     ScavTrap beta("beta");
 
+    FragTrap frag1("Frag-1");
+    FragTrap frag2("Frag-2");
+
     beta.takeDamage(alpha.meleeAttack(beta.getName()));
     beta.showStatus();
     beta.takeDamage(alpha.rangedAttack(beta.getName()));
@@ -20,5 +23,12 @@ int main(void)
         alpha.challengeNewcomer();
     }
 
+    // random attack(vaulthuner_dot_exe)
+    for (int i = 0; i < 3; i++)
+    {
+        frag2.takeDamage(frag1.vaulthunter_dot_exe(frag2.getName()));
+        frag1.showStatus();
+        frag2.showStatus();
+    }
     return (0);
 }
